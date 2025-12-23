62be2bbba8a1d1a439dd8a016a074ffffacf9029
Hrithvik S <hrithsnair90@gmail.com>
2025-12-23 12:46:15 +0800

    Add condition evaluation for input variables

--- k8s-terraform/variables.tf
+++ k8s-terraform/variables.tf
@@ -11,11 +11,21 @@
 variable "main_aws_region" {
   description = "The AWS region to deploy the cluster into"
   type        = string
+
+  validation {
+    condition     = var.main_aws_region == "ap-southeast-1"
+    error_message = "The AWS region must be 'ap-southeast-1'."
+  }
 }
 
 variable "main_environment" {
   description = "The deployment environment (e.g., dev, qa, prod)"
   type        = string
+
+  validation {
+    condition     = contains(["dev", "qa", "prod"], var.main_environment)
+    error_message = "Environment must be one of: dev, qa, prod."
+  }
 }
 
 variable "main_default_tags" {
@@ -26,6 +36,11 @@ variable "main_default_tags" {
 variable "main_cluster_name" {
   description = "The name of the Kubernetes cluster"
   type        = string
+
+  validation {
+    condition     = contains(["dev", "qa", "prod"], var.main_cluster_name)
+    error_message = "Cluster name must be one of: dev, qa, prod."
+  }
 }
 
 variable "main_project_prefix" {
@@ -39,65 +54,78 @@ variable "main_project_prefix" {
 variable "network_vpc_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
+
+  validation {
+    condition     = can(cidrhost(var.network_vpc_cidr, 0))
+    error_message = "The VPC CIDR block must be a valid IPv4 CIDR."
+  }
 }
 
 variable "network_public_subnets_cidr" {
   description = "List of CIDR blocks for public subnets"
   type        = list(string)
+
+  validation {
+    condition     = alltrue([for cidr in var.network_public_subnets_cidr : can(cidrhost(cidr, 0))])
+    error_message = "All public subnet CIDRs must be valid IPv4 CIDR blocks."
+  }
 }
 
 variable "network_private_subnets_cidr" {
   description = "List of CIDR blocks for private subnets"
   type        = list(string)
+
+  validation {
+    condition     = alltrue([for cidr in var.network_private_subnets_cidr : can(cidrhost(cidr, 0))])
+    error_message = "All private subnet CIDRs must be valid IPv4 CIDR blocks."
+  }
 }
 
 variable "network_availability_zones" {
   description = "List of availability zones to distribute subnets across"
   type        = list(string)
+
+  validation {
+    condition     = alltrue([for az in var.network_availability_zones : contains(["ap-southeast-1a", "ap-southeast-1b"], az)])
+    error_message = "Availability zones must be 'ap-southeast-1a' or 'ap-southeast-1b'."
+  }
 }
 
 /*
-* Control plane compute module configuration variables
+* Compute module configuration variables
 */
+
 variable "compute_control_plane_iam_role_policy" {
   description = "The IAM role policy for the control plane instances"
   type        = any
 }
 
-variable "compute_control_plane_ami_id" {
-  description = "The AMI ID for the control plane instances"
-  type        = string
-}
-
-variable "compute_control_plane_instance_type" {
-  description = "The EC2 instance type for the control plane instances"
-  type        = string
-}
-
-variable "compute_control_plane_key_name" {
-  description = "The SSH key to use for the control plane instance."
-  type        = string
-}
-
-/*
-* Worker node compute module configuration variables
-*/
 variable "compute_worker_nodes_iam_role_policy" {
   description = "The IAM role policy for the worker node instances"
   type        = any
 }
 
-variable "compute_worker_nodes_ami_id" {
-  description = "The AMI ID for the worker node instances"
+variable "compute_k8s_instance_ami_id" {
+  description = "The AMI ID for the K8s instances"
   type        = string
+
+  validation {
+    condition     = can(regex("^ami-[a-z0-9]+$", var.compute_k8s_instance_ami_id))
+    error_message = "The AMI ID must be a valid AWS AMI ID starting with 'ami-'."
+  }
 }
 
-variable "compute_worker_nodes_instance_type" {
-  description = "The EC2 instance type for the worker node instances"
+variable "compute_k8s_instance_type" {
+  description = "The EC2 instance type for the K8s instances"
   type        = string
+
+  validation {
+    condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.compute_k8s_instance_type)
+    error_message = "Instance type must be t3.micro, t3.small, or t3.medium."
+  }
 }
 
-variable "compute_worker_nodes_key_name" {
-  description = "The SSH key to use for the worker node instance."
+variable "compute_k8s_instance_ssh_key_name" {
+  description = "The SSH key for the K8s instances"
   type        = string
 }
